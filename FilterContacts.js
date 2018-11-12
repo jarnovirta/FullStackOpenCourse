11a85f84290434a12a7a49fc0532631e90b3cae5
Jarno Virta <jarnovirta@users.noreply.github.com>
2018-11-13 01:07:50 +0200

    Cleanup

--- FilterContacts.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Filter = () => {
-    return (<div>
-        rajaa näytettäviä: : <input value={this.state.seach} 
-              onChange={this.handleSearchInput} />
-    </div>)
-}
-export default Filter
