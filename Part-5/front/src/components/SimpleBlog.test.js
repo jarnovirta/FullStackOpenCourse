5a5cc5124064e296ea9da972e5c5f74dbf379a4e
Jarno Virta <jarnovirta@users.noreply.github.com>
2018-12-06 13:21:32 +0200

    Finished assignment 5.13

--- Part-5/front/src/components/SimpleBlog.test.js
+++ Part-5/front/src/components/SimpleBlog.test.js
@@ -4,22 +4,27 @@ import Blog from './Blog'
 import SimpleBlog from './SimpleBlog';
 
 describe.only('<SimpleBlog />', () => {
+    const blog = {
+        title: "test title",
+        author: "some author",
+        likes: 4
+    }
+    const mockClickHandler = jest.fn()
+    const blogComponent = shallow(<SimpleBlog
+        blog={blog}
+        onClick={mockClickHandler}
+        />)
+    const titleDiv = blogComponent.find('.title')
+    const contentDiv = blogComponent.find('.content')
     it('renders blog, author, likes', () => {
-        const blog = {
-            title: "test title",
-            author: "some author",
-            likes: 4
-        }
-        const onClickHandler = jest.fn()
-        const blogComponent = shallow(<SimpleBlog
-            blog={blog}
-            onClick={jest}
-            />)
-        const titleDiv = blogComponent.find('.title')
-        const contentDiv = blogComponent.find('.content')
         expect(titleDiv.text()).toContain(blog.title)
         expect(titleDiv.text()).toContain(blog.author)
         expect(contentDiv.text()).toContain(blog.likes)
     })
-
+    it('registers like clicks', () => {
+        const likeButton = contentDiv.find('button')
+        likeButton.simulate('click')
+        likeButton.simulate('click')
+        expect(mockClickHandler.mock.calls.length).toBe(2)
+    })
 })
